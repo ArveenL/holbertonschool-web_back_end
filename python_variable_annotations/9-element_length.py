f58957114551ec4d2b930b929391b2231fcfc682
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-04-08 15:03:26 +0400

    9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Annotate the below function's parameters
+and return values with the appropriate types
+
+def element_length(lst):
+    return [(i, len(i)) for i in lst]"""
+
+from typing import List, Tuple
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    return [(i, len(i)) for i in lst]

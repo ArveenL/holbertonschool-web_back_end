2d4af62b1ae13513488ffe34d680bb407c9db1b9
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-04-08 12:20:18 +0400

    1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""Write a type-annotated function concat that takes
+a string str1 and a string str2 as arguments and
+returns a concatenated string"""
+
+
+def concat(str1: float, str2: float) -> str:
+    """this is a long note"""
+
+    return (str1 + str2)

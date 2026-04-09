586bb5c73681dd92d4328d54e6d4b0d06bc2db62
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-04-09 14:31:11 +0400

    2

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,7 @@ because of concurrency."""
 import asyncio
 from typing import List
 
-wait_random = __import__('0-basics_async_syntax.py').wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:

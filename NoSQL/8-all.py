a4b65ac0829238c767afd3741cc0ab700ae0e39f
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-04-28 14:45:45 +0400

    8

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
 def list_all(mongo_collection):
-    """lists all documents in a collection"""
+    """Lists all documents in a collection"""
     return list(mongo_collection.find())

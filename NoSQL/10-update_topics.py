0d0a4d2494c0d5c268557ebb5d7d112cb1f6aed3
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-04-29 11:51:12 +0400

    11

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -11,3 +11,6 @@ def update_topics(mongo_collection, name, topics):
         { "name": name },
         { "$set": { "topics": topics } }
     )
+
+
+
---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Write a Python function that returns the list of school having a specific topic:"""
+
+
+from pymongo import MongoClient
+
+def schools_by_topic(mongo_collection, topic):
+    """"""Returns a list of schools having a specific topic""""""
+    return list(mongo_collection.find({"topics" : topic })

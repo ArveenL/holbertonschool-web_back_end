267ec4d4f07943f1f8010d60c0b1e1e628445133
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-04-29 08:30:03 +0400

    10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -2,12 +2,12 @@
 """Write a Python function that changes all topics of
 a school document based on the name:"""
 
-from pymongo import MongoClient
 
+from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
     """docstring"""
     mongo_collection.update_many(
-        { "name" : name},
-        { $set: { "topics" : topics } }
+        { "name" = name},
+        { $set: { "topics" = topics } }
     )

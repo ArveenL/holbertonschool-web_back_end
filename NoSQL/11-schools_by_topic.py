e8955a916d6d69b79cfaebc2a517364dc4eee4cb
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-04-29 11:54:34 +0400

    11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,5 +5,5 @@
 from pymongo import MongoClient
 
 def schools_by_topic(mongo_collection, topic):
-    """"""Returns a list of schools having a specific topic""""""
+    """"Returns a list of schools having a specific topic"""
     return list(mongo_collection.find({"topics" : topic }))

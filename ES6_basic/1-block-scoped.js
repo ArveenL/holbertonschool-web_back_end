4ad1b5c0a8ecae3be435cc9c018d5b6b8f228e91
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-04-30 11:47:25 +0400

    1

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+export default function taskBlock(trueOrFalse) {
+  var task;
+  task = false;
+  var task2;
+  task2 = false;
+
+
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+
+  return [task, task2];
+}

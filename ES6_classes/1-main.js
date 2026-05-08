37a5e86846cffae39b38cee0017006b39bbb6bd3
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-05-08 11:39:14 +0400

    1

--- ES6_classes/1-main.js
+++ ES6_classes/1-main.js
@@ -2,14 +2,14 @@ import ClassRoom from './0-classroom'
 
 export default function initializeRooms() {
     return [
-        new initializeRooms(19),
-        new initializeRooms(20),
-        new initializeRooms(34),
+        new Classroom(19),
+        new Classroom(20),
+        new Classroom(34),
     ];
 }
 
 // export default = allows to share the class with other files
 // function initializeRooms() = declaring a function and assigning its name
 // function initializeRooms = the function that will help create new classrooms with different student sizes
-// new = creating a new classroom with x amount of students, using the initializeRooms function
+// new Classroom = creating a new classroom with x amount of students, using the initializeRooms function
 // [] = to create a list of arrays i.e classroom via the initializeRooms
